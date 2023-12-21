3c0f725fc542a27065216b01ecda5bc2a368e7d3
Zohir Bala <zohirbala@Zohirs-Air.broadband>
2023-12-21 23:01:08 +0000

    Individual checks working. Multiple checks still needs to be applied

--- index.js
+++ index.js
@@ -1,58 +1,64 @@
 document.addEventListener('DOMContentLoaded', function() {
     const submitButton = document.getElementById("button");
-    const dayInput = document.querySelector(".day-block");
-    const monthInput = document.querySelector(".month-block");
-    const yearInput = document.querySelector(".year-block");
+    const dayInput = document.querySelector(".day-block .days");
+    const monthInput = document.querySelector(".month-block .months");
+    const yearInput = document.querySelector(".year-block .years");
 
-    function userInput(event) {
-        event.preventDefault(); // Prevent the default form submission
+    // function userInput(event) {
+    //     event.preventDefault(); // Prevent the default form submission
+    //     validate(dayInput, monthInput, yearInput);
+    // }
+
+    submitButton.addEventListener("click", (e)=>{
+        e.preventDefault(); // Prevent the default form submission
+        validate(dayInput, monthInput, yearInput);
+    });
+
+    function validate(day, month, year){
+
+        const inputs = [year, month, day]
+        const isDateInMonth = new Date(year.value, month.value, 0).getDate();
+        const isYearInFuture = new Date().getFullYear();
         
-        const dayBlock = dayInput.querySelector(".days");
-        const monthBlock = monthInput.querySelector(".months");
-        const yearBlock = yearInput.querySelector(".years");
-        
-        const dateInput = [yearBlock, monthBlock, dayBlock];
-        checkEmptyInputs(dateInput);
-        validateDate(yearBlock, monthBlock, dayBlock);
-    }
-    
-    submitButton.addEventListener("click", userInput);
-    
-    function checkEmptyInputs(inputs){
         inputs.forEach(input => {
-            const error = input.parentElement.querySelector('.error-message');
-            const label = input.parentElement.querySelector("label");
             
-            if(input.value === ""){
-                input.style.border = "1px solid red";
-                error.textContent = "Please fill in this field";
-                label.style.color = "var(--Light-red)";
-            } else{
+            if(!input.value){
+                handleValidationError(input, "Please fill in this field");
+            } else if(month.value > 12){
+                handleValidationError(month, "Must be a valid month");
+            } else if(day.value < 1 || day.value > isDateInMonth){
+                handleValidationError(day, "Must be a valid day");
+            } else if(year.value > isYearInFuture){
+                handleValidationError(year,"Must be in the past day");
+            } else {
+                input.parentElement.querySelector(".error-message").textContent = "";
                 input.style.border = "";
-                error.textContent = "";
-                label.style.color = "";
-            }
+                input.parentElement.querySelector("label").style.color = "";
+            } 
         });
-    }
+    };
     
-    function validateDate(year, month, day){
-        yearProvided = year.value;
-        monthProvided = month.value;
-        dayProvided = day.value;
+    function handleValidationError(result, message) {
+        const error = result.parentElement.querySelector('.error-message');
+        const label = result.parentElement.querySelector("label");
 
-        errorMessage = monthInput.querySelector(".error-message");
-        monthBox = monthInput.querySelector("input");
-        monthLabel = monthInput.querySelector("label");
-        
-        const dateString = `${yearProvided}-${monthProvided.padStart(2, '0')}-${dayProvided.padStart(2, '0')}`;
-        const date = new Date(dateString);
-        
-        if (!(date instanceof Date) || isNaN(date)){
-            if(monthProvided < 1 || monthProvided > 12){
-                errorMessage.textContent = "Incorrect month";
-                monthBox.style.border = "1px solid var(--Light-red)";
-                monthLabel.style.color = "var(--Light-red)";
-            };
-        }
+        result.style.border = "1px solid var(--Light-red)";
+        error.textContent = message;
+        label.style.color = "var(--Light-red)";
     }
+
+    // function resetValidation(result, error, label) {
+    //     result.style.border = "";
+    //     error.textContent = "";
+    //     label.style.color = "";
+    // }
 });
+
+
+/* 
+I need a variable to count as false and give situations to set it to false
+rather than the if the date is invalid 
+
+If the year in invalid then say enter a correct date, if not check the date is valid if not 
+specify if the day is what is incorrect or the month or both
+*/
